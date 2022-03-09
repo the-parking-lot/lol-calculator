9afedd79749c68045b8e825a57730269227d6451
Marcus O'Yang <18moyang@gmail.com>
2022-03-09 17:40:14 +1100

    added boxes

--- pages/calculator.tsx
+++ pages/calculator.tsx
@@ -1,11 +1,51 @@
 import {
   Box,
+  Flex,
+  Heading,
+  HStack,
+  Spacer,
+  useColorMode,
+  useColorModeValue,
 } from '@chakra-ui/react';
 
 const calculator = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <>
       <Box>
+        <Flex>
+          <Flex w="100%" mt={2} h={400}>
+            <Box 
+              bg={useColorModeValue('gray.100', 'gray.900')}
+              py={2}
+              maxW="75%"
+              w="75%"
+              borderRadius="5px"
+              ml={2}
+              mr={1}
+            >
+              <Heading
+                size='md'
+                ml={3}
+                >Champion Info</Heading>
+            </Box>
+            <Box 
+              bg={useColorModeValue('gray.100', 'gray.900')}
+              borderRadius="5px"
+              py={2}
+              maxW="25%"
+              w="25%"
+              ml={1}
+              mr={2}
+            >
+            <Heading
+              size='md'
+              ml={3}
+              >Champions List</Heading>
+            </Box>
+          </Flex>
+        </Flex>
       </Box>
     </>
   )
