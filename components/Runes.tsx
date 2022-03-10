1e58654fef1d74634940a5bfd673090a4fa3c842
Marcus O'Yang <18moyang@gmail.com>
2022-03-10 20:36:23 +1100

    n

--- components/Runes.tsx
+++ components/Runes.tsx
@@ -2,7 +2,7 @@ import { Box, Flex, Heading, Spacer, useColorMode, useColorModeValue, useDisclos
 
 const Runes = () => {
     return (
-        <Box bg={useColorModeValue('gray.200', 'gray.700')} p={4} minW={500} w="96.6%" borderRadius={5} alignItems={"center"} textAlign={"center"} >
+        <Box bg={useColorModeValue('gray.200', 'gray.700')} minW={500} w="96.6%" mx={10} borderRadius={5} textAlign={"center"} >
             <Heading size="lg" mb={3}>Runes</Heading>
             <Flex w="100%" h={675} my={4} direction={"row"}>
                 <Box bg={useColorModeValue('white', 'gray.800')} flex='1' h="100%" borderRadius={5} mx={5}>
